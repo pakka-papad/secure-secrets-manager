84cd598fb08c8c4b2987c627323b561df67769ee
pakka-papad <76241334+pakka-papad@users.noreply.github.com>
2026-01-07 00:09:32 +0530

    init

---
+++ src/main/java/com/example/secrets_manager/SecretsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.secrets_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretsManagerApplication.class, args);
+	}
+
+}
