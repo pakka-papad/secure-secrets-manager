294af8f6e9c1332a703e75027eec71c91f21340a
pakka-papad <76241334+pakka-papad@users.noreply.github.com>
2026-01-11 01:03:16 +0530

    Update: Flyway works

--- src/main/java/com/example/secrets_manager/SecretsManagerApplication.java
+++ src/main/java/com/example/secrets_manager/SecretsManagerApplication.java
@@ -3,10 +3,13 @@ package com.example.secrets_manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SecretsManagerApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(SecretsManagerApplication.class, args);
 	}
 
