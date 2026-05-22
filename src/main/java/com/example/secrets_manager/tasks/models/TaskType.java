274591722003aec12d62c7e5e117d1dce997d515
pakka-papad <76241334+pakka-papad@users.noreply.github.com>
2026-05-23 03:19:03 +0530

    Remove unused TaskType

--- src/main/java/com/example/secrets_manager/tasks/models/TaskType.java
+++ src/main/java/com/example/secrets_manager/tasks/models/TaskType.java
@@ -2,6 +2,5 @@ package com.example.secrets_manager.tasks.models;
 
 /** Defines all possible types of background tasks in the system. */
 public enum TaskType {
-  MASTER_KEY_MIGRATION,
-  SECRET_ROTATION
+  MASTER_KEY_MIGRATION
 }
